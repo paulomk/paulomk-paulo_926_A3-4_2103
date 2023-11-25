5129a4b29ee06de2f90dc23cfd160c640bda3636
PaulomK <paulo.massao.kawakami@gmail.com>
2023-11-24 23:44:47 -0500

    implement the view, styles and the function to convert the values

--- conversion.js
+++ conversion.js
@@ -1,1 +1,7 @@
 //JS File
+let calculate = function(){
+    let exchangeRate = .75;
+    let amountUSD = parseFloat(document.getElementById("amount").value);
+    let convertedCAD = amountUSD * exchangeRate;
+    document.getElementById("converted").value = convertedCAD.toFixed(2);
+}
